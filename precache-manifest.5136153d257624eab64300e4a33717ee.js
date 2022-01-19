16af666fc41720829aa38b3b20e864865fe06ce6
r4z4 <85771218+r4z4@users.noreply.github.com>
2022-01-18 21:30:57 -0600

    Add files via upload

---
+++ precache-manifest.5136153d257624eab64300e4a33717ee.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e648b15f0498a40eb6b63668571ab43",
+    "url": "/Capture.png"
+  },
+  {
+    "revision": "cace645f6a760547c514f0e519d95ea4",
+    "url": "/Capture1.png"
+  },
+  {
+    "revision": "1a4764c96bf354dbc3665ac8b13fb274",
+    "url": "/Capture2.png"
+  },
+  {
+    "revision": "a0723fd0737ac69d521822c1bcec66f8",
+    "url": "/Capture3.png"
+  },
+  {
+    "revision": "5309152b012c9d3af6444b3f3a0d9cc9",
+    "url": "/Capture4.png"
+  },
+  {
+    "revision": "c83d891186dfbc4690dd9f9fb2d0200c",
+    "url": "/Capture5.png"
+  },
+  {
+    "revision": "538ae682ec7a6c31b0445c6b537330b8",
+    "url": "/Capture6.png"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "6007858290f749fb4fbe690e8a2cfdb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "7a0eccb9b0f2d29bd42e",
+    "url": "/static/css/main.744817ad.chunk.css"
+  },
+  {
+    "revision": "7a0eccb9b0f2d29bd42e",
+    "url": "/static/js/main.7a0eccb9.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  },
+  {
+    "revision": "06d17ecc0f6dc7601bd0424d40139914",
+    "url": "/static/media/test.4ce1ab12.png"
+  }
+]);
